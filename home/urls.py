8e57d9108b8a6af0413e260e5aedd7b322f0e49f
Arman <smesharm85@gmail.com>
2023-01-23 17:27:05 +0500

    Added main pages and style

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("check-page", views.check, name="check-page"),
+    path("tested-sites", views.tested, name="tested-sites"),
+    path("useful-info", views.useful, name="useful-info")
 ]
--- home/views.py
+++ home/views.py
@@ -2,4 +2,40 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "home/index.html", {'obj': 'Домашняя страница'})
+    return render(request, "home/index.html", {
+        'obj': 'Домашняя страница',
+        'home': True,
+        'check': False,
+        'useful': False,
+        'tested': False
+        })
+
+def check(request):
+    return render(request, "home/check_page.html", {
+        'obj': 'Проверка сайта',
+        'home': False,
+        'check': True,
+        'useful': False,
+        'tested': False
+        })
+    
+def useful(request):
+    return render(request, "home/useful.html", {
+        'obj': 'Полезная информация',
+        'home': False,
+        'check': False,
+        'useful': True,
+        'tested': False
+        })
+    
+def tested(request):
+    return render(request, "home/tested.html", {
+        'obj': 'Проверенные сайты',
+        'home': False,
+        'check': False,
+        'useful': False,
+        'tested': True
+        })
+
+
+
